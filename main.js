cd37c8310d724af5d956c8b2d4217416e63097dd
abhishekbansal131 <90637721+abhishekbansal131@users.noreply.github.com>
2022-12-10 15:30:34 +0530

    Add files via upload
    Initial Commit

---
+++ main.js
@@ -0,0 +1,94 @@
+  var c = 12;
+  var d = 6;
+  if(c <= 0){
+    alert('game over');
+    location.reload();
+    
+    document.getElementById('try').innerHTML = `Try's Remaining: ` + Math.ceil(c/2);
+  }
+  else{
+    
+  document.getElementById('try').innerHTML = `Try's Remaining: ` + Math.ceil(c/2);
+  }
+  const cards = document.querySelectorAll('.memory-card');
+
+  let hasFlippedCard = false;
+  let lockBoard = false;
+  let firstCard, secondCard;
+//   if(c==0){
+//     alert('game over');
+//     location.reload();
+//     c=6;
+//     document.getElementById('try').innerHTML = `Try's Remaining: ` + Math.ceil(c/2);
+//   }
+//   else{
+
+//   document.getElementById('try').innerHTML = `Try's Remaining: ` + Math.ceil(c/2);
+// }
+
+
+  function flipCard() {
+    if(c==0){
+      alert('game over');
+      location.reload();
+      document.getElementById('try').innerHTML = `Try's Remaining: ` + Math.ceil(c/2);
+    }
+    else{
+      
+    document.getElementById('try').innerHTML = `Try's Remaining: ` + Math.ceil(c/2);
+  c = c-1;}
+    
+
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+      hasFlippedCard = true;
+      firstCard = this;
+      return;
+    }
+
+    secondCard = this;
+    lockBoard = true;
+
+    checkForMatch();
+  }
+
+  function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    // c = c-1;
+    isMatch ? disableCards() : unflipCards();
+
+  }
+
+  function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    c = c+1;
+    resetBoard();
+  }
+
+  function unflipCards() {
+    setTimeout(() => {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+
+      resetBoard();
+    }, 1500);
+  }
+
+  function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+  }
+
+ (function shuffle() {
+   cards.forEach(card => {
+     let ramdomPos = Math.floor(Math.random() * 12);
+     card.style.order = ramdomPos;
+   });
+ })();
+
+  cards.forEach(card => card.addEventListener('click', flipCard));
